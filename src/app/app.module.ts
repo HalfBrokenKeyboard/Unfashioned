f739d479943a98d8bcccbd76884c81e27cc9d24c
HalfBrokenKeyboard <lucasandersen2003@gmail.com>
2023-12-15 17:29:21 +0100

    more stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {CommonModule} from "@angular/common";
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MapComponent } from './map/map.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    MapComponent,
   ],
   imports: [
     BrowserModule,
