eed63bb4f82d4e94fc2dbeb7dd989bebd1f69ffd
HalfBrokenKeyboard <lucasandersen2003@gmail.com>
2024-01-15 20:05:05 +0100

    Vercel environment

--- src/app/core/services/stripe.service.ts
+++ src/app/core/services/stripe.service.ts
@@ -1,8 +1,8 @@
 // stripe.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
-// import { environment } from 'src/environments/environment.prod';
+//import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root',
